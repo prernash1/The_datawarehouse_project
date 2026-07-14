0f68a2dd95738090a02a8e0fbae7fb80096aa87a
Prernasunil <prerna.sharma3107@gmail.com>
2026-07-15 00:24:11 +0530

    Implemented transformations to the sales data in the silver layer and updated the DDL structure of it.

--- sqlscripts/Silver/load_silver.sql
+++ sqlscripts/Silver/load_silver.sql
@@ -7,10 +7,10 @@ INSERT INTO silver_crm_cust_info(
 	cst_lastname, 
 	cst_marital_status, 
 	cst_gndr, 
-	cst_create_date
+	cst_create_date  
 )
 SELECT 
-	cst_id, 
+	cst_id,    
 	cst_key,
 	TRIM(cst_firstname) as cst_firstname, -- removed unwanted spaces using TRIM()
 	TRIM(cst_lastname) as cst_lastname,
@@ -63,3 +63,42 @@ CAST(prd_start_dt AS DATE) AS prd_start_dt,
 -- calculate end date as one day before the next start date
 LEAD(CAST(prd_start_dt AS DATE)) OVER (PARTITION BY prd_key ORDER BY CAST(prd_start_dt AS DATE)) - INTERVAL 1 DAY AS prd_end_dt
 FROM bronze_crm_prd_info;
+
+
+-- crm_sales_details table
+TRUNCATE TABLE silver_crm_sales_details;
+INSERT INTO silver_crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+	sls_quantity,
+    sls_price
+)
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt <= 0 OR LENGTH(sls_order_dt) <> 8 THEN NULL
+     ELSE STR_TO_DATE(sls_order_dt,'%Y%m%d')
+END AS sls_order_dt,
+CASE WHEN sls_ship_dt <= 0 OR LENGTH(sls_ship_dt) != 8 THEN NULL
+     ELSE STR_TO_DATE(sls_ship_dt,'%Y%m%d')
+END AS sls_ship_dt,
+CASE WHEN sls_due_dt <= 0 OR LENGTH(sls_due_dt) != 8 THEN NULL
+     ELSE STR_TO_DATE(sls_due_dt,'%Y%m%d')
+END AS sls_due_dt,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+     THEN sls_quantity * ABS(sls_price)
+     ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0
+     THEN sls_sales/NULLIF(sls_quantity,0)
+     ELSE ABS(sls_price)
+END AS sls_price
+FROM bronze_crm_sales_details;
+
