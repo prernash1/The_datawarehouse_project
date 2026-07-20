c33e9fee377af3f9952d71df923da5cb5d126be6
Prernasunil <prerna.sharma3107@gmail.com>
2026-07-20 17:56:35 +0530

    Completed GOLD layer

--- sqlscripts/Gold/ddl_gold.sql
+++ sqlscripts/Gold/ddl_gold.sql
@@ -1,3 +1,6 @@
+-- Dimension 1: gold_customer_dim
+
+DROP VIEW IF EXISTS gold_customer_dim;
 CREATE VIEW gold_customer_dim AS
 	SELECT
 	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
@@ -20,8 +23,48 @@ CREATE VIEW gold_customer_dim AS
 	ON ci.cst_key = la.cid
 ;
 
+-- Dimension 2: gold_product_dim
 
+DROP VIEW IF EXISTS gold_product_dim;
+CREATE VIEW gold_product_dim AS
+SELECT 
+ROW_NUMBER() OVER(ORDER BY pi.prd_id) AS product_key, -- surrogate key
+pi.prd_id AS product_id,
+pi.prd_key AS product_code,
+pi.prd_nm AS product_name,
+pi.prd_line AS product_line,
+pi.prd_cost AS cost,
+pi.cat_id AS category_id,
+pc.cat AS category,
+pc.subcat AS subcategory,
+pc.maintenance AS maintenance,
+pi.prd_start_dt AS start_date
+FROM silver_crm_prd_info pi
+LEFT JOIN silver_erp_px_cat_g1v2 pc
+ON pi.cat_id = pc.id
+WHERE pi.prd_end_dt IS NULL -- To filter out historical data because a null end date means product is active
+;
 
+-- Fact Table : gold_sales_fact
+
+DROP VIEW IF EXISTS gold_sales_fact;
+CREATE VIEW gold_sales_fact AS
+SELECT 
+sd.sls_ord_num AS order_number,
+pd.product_key,
+cd.customer_key,
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_quantity AS quantity,
+sd.sls_sales AS sales_amount,
+sd.sls_price AS price
+FROM silver_crm_sales_details sd
+LEFT JOIN gold_product_dim pd
+ON sd.sls_prd_key = pd.product_code
+LEFT JOIN gold_customer_dim cd
+ON sd.sls_cust_id = cd.customer_id
+;
 
 
 
